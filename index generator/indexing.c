f56af15e6eeaab4fdf6ffd1598ea1e7a40bd1ab2
veim <veiveim@163.com>
2017-05-18 23:32:44 +0800

    find lyrics in string

--- index generator/indexing.c
+++ index generator/indexing.c
@@ -7,22 +7,40 @@
 
 
 #define DATA_FILE_SUFFIX	".data"
-#define JSON_TOKEN_NUM		256
+#define JSON_TOKEN_NUM		512
 
 
 
-/* parse a line in json type */
-int parse(const char* str){
+/* parse a line in json type, set the position of lyrics */
+/* return 1 if lyrics found, otherwise return 0 */
+int find_lyrics(const char* line, int* start, int* end){
 	jsmn_parser parser;
 	jsmntok_t tokens[JSON_TOKEN_NUM];
 
 	jsmn_init(&parser);
 
-	int cnt = jsmn_parse(&parser, str, strlen(str), tokens, JSON_TOKEN_NUM);
+	int cnt = jsmn_parse(&parser, line, strlen(line), tokens, JSON_TOKEN_NUM);
 
 	if(cnt < 0){
-		printf("jsmn parse token cnt = %d\n", cnt);
-		return 1;
+		printf("Error: jsmn parsing failed, returned %d\n", cnt);
+		return 0;
+	}
+
+	int i;
+	for(i = 0; i < cnt; i++){
+		int size = tokens[i].end - tokens[i].start + 1;
+		char* str = (char*) malloc(size + 1);
+		snprintf(str, size, "%s", (char*)(line + tokens[i].start));
+	
+		/* check if the find the token "lyrics" */
+		if(!strncmp(str, "lyrics", 6)){
+			*start = tokens[i+1].start;
+			*end = tokens[i+1].end;
+			free(str);
+			return 1;
+		}
+		
+		free(str);
 	}
 
 	return 0;
@@ -30,7 +48,7 @@ int parse(const char* str){
 
 
 /* check whether str ended with suffix */
-int EndWith(const char* str, const char* suffix){
+int suffix(const char* str, const char* suffix){
 	if(!str || !suffix)
 		return 0;
 
@@ -60,7 +78,7 @@ int main(int argc, char **args){
 	struct dirent* pEnt;
 	while((pEnt = readdir(pDir)) != NULL){
 		/* skip non-use files */
-		if(!EndWith(pEnt->d_name, DATA_FILE_SUFFIX))
+		if(!suffix(pEnt->d_name, DATA_FILE_SUFFIX))
 			continue;
 
 		/* open useful file */
@@ -87,12 +105,18 @@ int main(int argc, char **args){
 		
 		//printf("aligned size:%d\n%s\n", size, line);
 
-		if(parse(line)){
-			printf("file name: %s\n", pEnt->d_name);
+		/* start & end pos of lyrics */
+		int lyrics_start, lyrics_end;
+
+		if(!find_lyrics(line, &lyrics_start, &lyrics_end)){
+			printf("Error: no lyrics found in file: %s\n", pEnt->d_name);
+			continue;
 		}
 
-
-
+		int lyrics_len = lyrics_end - lyrics_start + 1;
+		char* str = (char*) malloc(lyrics_len + 1);
+		snprintf(str, size, "%s", (char*)(line + lyrics_start));
+		printf("%s\n\n", str);
 
 		
 		free(line);
