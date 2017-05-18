0a0b7af7fd5a9e4bcbfb673b490cb5b420f5a874
veim <veiveim@163.com>
2017-05-18 22:15:18 +0800

    add indexing

---
+++ index generator/indexing.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+
+#include "jsmn/jsmn.h"
+
+
+#define DATA_FILE_SUFFIX	".data"
+
+
+int EndWith(const char* str, const char* suffix){
+	if(!str || !suffix)
+		return 0;
+
+	size_t lenstr = strlen(str);
+	size_t lensuffix = strlen(suffix);
+	if(lensuffix > lenstr)
+		return 0;
+	return strncmp(str + lenstr - lensuffix, suffix, lensuffix) == 0;
+}
+
+
+int main(int argc, char **args){
+	
+	if(argc == 1){
+		printf("Error: no directory parameter given\n");
+		return 1;
+	}
+
+	/* parsing files in the dedicated directory */	
+	DIR* pDir = opendir(args[1]);		
+	if(pDir == NULL){
+		printf("Error: cannot open directory %s\n", args[1]);
+		return 1;
+	}
+
+	int cnt = 0;
+	struct dirent* pEnt;
+	while((pEnt = readdir(pDir)) != NULL){
+		/* skip non-use files */
+		if(!EndWith(pEnt->d_name, DATA_FILE_SUFFIX))
+			continue;
+
+		/* open useful file */
+		char path[128];
+		sprintf(path, "%s/%s", args[1], pEnt->d_name);
+		FILE* pFile = fopen(path, "r");	
+		if(pFile == NULL){
+			printf("Error: cannot open file %s\n", pEnt->d_name);
+			continue;
+		}
+
+		/* read line in opened file */
+		fseek(pFile, 0L, SEEK_END);
+		int size = (size_t) ftell(pFile);
+		size = (size / 8 + 1) * 8; /* align to 8 */ 
+		fseek(pFile, 0L, SEEK_SET);
+
+		char* line = (char*) malloc(size);
+		if(fgets(line, size, pFile) == NULL){
+			printf("Error: reading lines failed\n");
+			free(line);
+			continue;
+		}
+
+		
+		printf("aligned size:%d\n%s\n", size, line);
+		
+		free(line);
+		fclose(pFile);
+
+		cnt ++;
+		if(cnt == 128)
+			break;
+	}
+
+	closedir(pDir);
+	return 0;
+}
